4fbcae71d2ff997cf7f7d0140ef23c5fb0c40e18
Tuna <Tdb00096@gmail.com>
2023-06-07 20:50:17 -0400

    adding CORS

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 const pool = require('./db');
 const bcrypt = require('bcrypt');
+const cors = require('cors')
 
+app.use(cors())
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port, () => console.log(`Server running at http://localhost:${port}`));
