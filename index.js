2cfc865231c6829995140e33b59adeb54006e271
Tuna <Tdb00096@gmail.com>
2023-06-07 20:29:31 -0400

    creating users table

--- index.js
+++ index.js
@@ -7,6 +7,23 @@ app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port, () => console.log(`Server running at http://localhost:${port}`));
 
+pool.query(`
+  CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    birthdate DATE NOT NULL
+  );
+`, (err, res) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log('Table successfully created');
+});
+
+
 app.get('/users', async (req, res) => {
     try {
       const client = await pool.connect();
