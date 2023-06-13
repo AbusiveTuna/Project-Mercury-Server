9a88c39a098a29837ee5991b91a9d8ffeea55331
AbusiveTuna <89785560+AbusiveTuna@users.noreply.github.com>
2023-06-13 10:15:01 -0400

    Update index.js

--- index.js
+++ index.js
@@ -32,6 +32,22 @@ pool.query(`
   console.log('Table successfully created');
 });
 
+pool.query(`
+  CREATE TABLE IF NOT EXISTS password_reset (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(255) NOT NULL,
+    code VARCHAR(6) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  );
+`, (err, res) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log('Table successfully created');
+});
+
+
 app.post('/addUser', async (req, res) => {
   const { username, password, email, birthdate } = req.body;
   try {
@@ -75,3 +91,109 @@ app.post('/login', async (req, res) => {
       res.status(500).json({ message: 'An error occurred during login' });
     }
 });
+
+const nodemailer = require('nodemailer');
+const Email = require('email-templates');
+
+app.post('/requestReset', async (req, res) => {
+  const { email } = req.body;
+
+  try {
+    // check if email exists in the database
+    const result = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
+
+    if (result.rows.length > 0) {
+      //generates a six-digit verification code
+      const code = Math.floor(100000 + Math.random() * 900000).toString();
+
+      await pool.query(
+        'INSERT INTO password_reset (email, code) VALUES ($1, $2)',
+        [email, code]
+      );
+
+      //update with EMAILJS stuff
+        //npm install @emailjs/browser --save
+      const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: process.env.EMAIL_USER,
+          pass: process.env.EMAIL_PASS
+        }
+      });
+
+      const emailTemplate = new Email({
+        transport: transporter,
+        send: true,
+        preview: false,
+      });
+
+      await emailTemplate.send({
+        template: 'passwordReset',
+        message: {
+          to: email
+        },
+        locals: {
+          code: code
+        }
+      });
+
+      res.status(200).json({ message: 'Verification code sent' });
+    } else {
+      res.status(404).json({ message: 'Email not found' });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'An error occurred while requesting a password reset' });
+  }
+});
+
+app.post('/verifyCode', async (req, res) => {
+  const { email, code } = req.body;
+
+  try {
+    const result = await pool.query(
+      'SELECT * FROM password_reset WHERE email = $1 AND code = $2',
+      [email, code]
+    );
+
+    if (result.rows.length > 0) {
+      const resetRequest = result.rows[0];
+
+      //check if 10 minutes have passed since the code was created
+      const createdAt = new Date(resetRequest.created_at);
+      const now = new Date();
+      const diff = (now - createdAt) / 1000 / 60;
+
+      if (diff <= 10) {
+        res.status(200).json({ message: 'Verification successful' });
+      } else {
+        res.status(400).json({ message: 'Verification code expired' });
+      }
+    } else {
+      res.status(400).json({ message: 'Verification code incorrect' });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'An error occurred during verification' });
+  }
+});
+
+app.post('/resetPassword', async (req, res) => {
+  const { email, newPassword } = req.body;
+
+  try {
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
+
+    await pool.query(
+      'UPDATE users SET password = $1 WHERE email = $2',
+      [hashedPassword, email]
+    );
+
+    res.status(200).json({ message: 'Password reset successful' });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'An error occurred while resetting the password' });
+  }
+});
+
+
