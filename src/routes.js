ec7c6a511882d1a60c7ab22eb5a3a907529a3685
AbusiveTuna <89785560+AbusiveTuna@users.noreply.github.com>
2023-07-10 08:35:23 -0400

    Updating with User ID

--- src/routes.js
+++ src/routes.js
@@ -74,31 +74,31 @@ router.post('/addUser', async (req, res) => {
  * Returns: 200 on valid credientials
 */
 router.post('/login', async (req, res) => {
-    let { username, password } = req.body;
-  
-    try {
-      const validatedInput = validateUserInput(username,password,"None@gmail.com","1990-01-01");
-      username = validatedInput.username;
-      password = validatedInput.password;
-      const result = await pool.query('SELECT * FROM users WHERE username = $1', [username]);
-  
-      if (result.rows.length > 0) {
-        const user = result.rows[0];
+  let { username, password } = req.body;
 
-        const match = await bcrypt.compare(password, user.password);
-  
-        if (match) {
-          res.status(200).json({ message: 'Login successful' });
-        } else {
-          res.status(401).json({ message: 'Invalid username or password' });
-        }
+  try {
+    const validatedInput = validateUserInput(username,password,"None@gmail.com","1990-01-01");
+    username = validatedInput.username;
+    password = validatedInput.password;
+    const result = await pool.query('SELECT * FROM users WHERE username = $1', [username]);
+
+    if (result.rows.length > 0) {
+      const user = result.rows[0];
+
+      const match = await bcrypt.compare(password, user.password);
+
+      if (match) {
+        res.status(200).json({ message: 'Login successful', user_id: user.id });
       } else {
         res.status(401).json({ message: 'Invalid username or password' });
       }
-    } catch (err) {
-      console.error(err);
-      res.status(500).json({ message: 'An error occurred during login' });
+    } else {
+      res.status(401).json({ message: 'Invalid username or password' });
     }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'An error occurred during login' });
+  }
 });
 
 
