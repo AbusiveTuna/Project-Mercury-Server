9e06fe1c4b35f1ee27289808eadc632258119eee
AbusiveTuna <89785560+AbusiveTuna@users.noreply.github.com>
2023-07-10 08:18:20 -0400

    Update tables.js
    add in key storage

--- src/db/tables.js
+++ src/db/tables.js
@@ -20,6 +20,16 @@ const createTables = async () => {
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
       );
     `);
+
+    await pool.query(`
+      CREATE TABLE IF NOT EXISTS dexcom_tokens (
+        id SERIAL PRIMARY KEY,
+        user_id INTEGER NOT NULL,
+        access_token VARCHAR(255) NOT NULL,
+        refresh_token VARCHAR(255) NOT NULL,
+        FOREIGN KEY (user_id) REFERENCES users(id)
+      );
+    `);
   } catch (err) {
     console.error(err);
   }
