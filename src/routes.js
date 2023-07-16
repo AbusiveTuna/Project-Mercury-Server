671436c2d0f3dfd707ca4874ea0cbd4f22f1dfb9
Tuna <Tdb00096@gmail.com>
2023-07-16 16:13:49 -0400

    updating with new routes

--- src/routes.js
+++ src/routes.js
@@ -254,7 +254,7 @@ router.post('/exchangeCode', async (req, res) => {
       },
       body: new URLSearchParams(body),
     });
-    
+
     const data = await response.json();
 
     const { access_token, refresh_token } = data;
@@ -273,6 +273,105 @@ router.post('/exchangeCode', async (req, res) => {
     res.status(500).json({ error: 'Failed to exchange code' });
   }
 });
+
+router.get('/getDexcomData/:userId', async (req, res) => {
+  const { userId } = req.params;
+
+  try {
+    // Get the user's access and refresh tokens from the database
+    const result = await pool.query('SELECT * FROM dexcom_tokens WHERE user_id = $1', [userId]);
+
+    if (result.rows.length > 0) {
+      const { access_token, refresh_token } = result.rows[0];
+      const dexcomData = await getDexcomData(access_token, refresh_token, userId);
+
+      if (dexcomData.error) {
+        res.status(500).json({ message: 'Error fetching Dexcom data', error: dexcomData.error });
+      } else {
+        res.json(dexcomData);
+      }
+    } else {
+      res.status(404).json({ message: 'No Dexcom tokens found for this user' });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server error');
+  }
+});
+
+async function getDexcomData(accessToken, refreshToken, userId) {
+  const date = new Date().toISOString();
+
+  try {
+    const response = await fetch(`https://api.dexcom.com/v3/users/self/egvs?startDate=${date}&endDate=${date}`, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${accessToken}`
+      }
+    });
+
+    if (response.status === 401) {
+      // Access token expired, refresh it
+      const refreshedToken = await refreshDexcomToken(refreshToken, userId);
+
+      if (refreshedToken.error) {
+        return { error: 'Failed to refresh Dexcom token' };
+      } else {
+        // Retry the Dexcom API call with the new access token
+        return await getDexcomData(refreshedToken, refreshToken, userId);
+      }
+    } else {
+      const data = await response.json();
+      return data;
+    }
+  } catch (error) {
+    console.error(error);
+    return { error: 'Failed to fetch Dexcom data' };
+  }
+}
+
+
+async function refreshDexcomToken(refreshToken, userId) {
+  const body = {
+    client_id: process.env.DEXCOM_CLIENT_ID,
+    client_secret: process.env.DEXCOM_SECRET,
+    refresh_token: refreshToken,
+    grant_type: 'refresh_token',
+    redirect_uri: 'Https://ProjectsMercury.com/dexcomRedirect',
+  };
+
+  try {
+    const response = await fetch('https://sandbox-api.dexcom.com/v2/oauth2/token', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: new URLSearchParams(body),
+    });
+
+    const data = await response.json();
+
+    const { access_token } = data;
+    if (access_token) {
+      // Store the new access token in the database
+      await pool.query(
+        'UPDATE dexcom_tokens SET access_token = $1 WHERE user_id = $2',
+        [access_token, userId]
+      );
+
+      return access_token;
+    } else {
+      console.error('Failed to refresh Dexcom token: No access token received');
+      return { error: 'Failed to refresh Dexcom token: No access token received' };
+    }
+  } catch (error) {
+    console.error(error);
+    return { error: 'Failed to refresh Dexcom token' };
+  }
+}
+
+
+
 
 
 
