45fb9db06955897922ad90f71be93df940af80ff
Tuna <Tdb00096@gmail.com>
2023-07-23 15:20:01 -0400

    reverting

--- src/db/db.js
+++ src/db/db.js
@@ -1,25 +1,25 @@
 // For Prod
-// const { Pool } = require('pg');
-
-// const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false
-//   }
-// });
-
-// module.exports = pool;
-
-//For Local Unit Tests
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: "REPLACE WITH TEST DB URL LOCALLY",
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   }
 });
 
 module.exports = pool;
+
+//For Local Unit Tests
+// const { Pool } = require('pg');
+
+// const pool = new Pool({
+//   connectionString: "REPLACE WITH TEST DB URL LOCALLY",
+//   ssl: {
+//     rejectUnauthorized: false
+//   }
+// });
+
+// module.exports = pool;
 
 
