8600234fc485bcb42ec6e8060e28c40d3f72be5a
AbusiveTuna <89785560+AbusiveTuna@users.noreply.github.com>
2023-07-24 07:52:11 -0400

    Update dexcomRoutes.test.js

--- src/tests/dexcomRoutes.test.js
+++ src/tests/dexcomRoutes.test.js
@@ -23,3 +23,26 @@ describe('POST /exchangeCode', () => {
   });
 });
 
+describe('GET /getDexcomData/:userId', () => {
+  it('should get Dexcom data for a user', async () => {
+    const res = await request(app)
+      .get('/getDexcomData/mockUserId');
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toEqual({ data: 'mockDexcomData' });
+  });
+
+  it('should handle error when getting Dexcom data', async () => {
+    const res = await request(app)
+      .get('/getDexcomData/mockUserId');
+    expect(res.statusCode).toEqual(500);
+    expect(res.body).toEqual({ message: 'Error fetching Dexcom data', error: 'mockError' });
+  });
+
+  it('should return 404 if no Dexcom tokens found for user', async () => {
+    const res = await request(app)
+      .get('/getDexcomData/mockUserId');
+    expect(res.statusCode).toEqual(404);
+    expect(res.body).toEqual({ message: 'No Dexcom tokens found for this user' });
+  });
+});
+
