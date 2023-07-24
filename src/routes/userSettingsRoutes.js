60f6b9e8715e44592fe81a466c892f9b2ba07348
AbusiveTuna <89785560+AbusiveTuna@users.noreply.github.com>
2023-07-24 08:24:30 -0400

    Update userSettingsRoutes.js

--- src/routes/userSettingsRoutes.js
+++ src/routes/userSettingsRoutes.js
@@ -1,1 +1,44 @@
+const express = require('express');
+const router = express.Router();
+const pool = require('../db/db');
+
+router.get('/getUserSettings/:userId', async (req, res) => {
+  const { userId } = req.params;
+
+  try {
+    const result = await pool.query('SELECT * FROM user_settings WHERE user_id = $1', [userId]);
+
+    if (result.rows.length > 0) {
+      res.json(result.rows[0]);
+    } else {
+      res.status(404).json({ message: 'User settings not found' });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'An error occurred while retrieving user settings' });
+  }
+});
+
+router.post('/updateUserSettings/:userId', async (req, res) => {
+  const { userId } = req.params;
+  const { highThreshold, lowThreshold } = req.body;
+
+  try {
+    const result = await pool.query(
+      'UPDATE user_settings SET high_threshold = $1, low_threshold = $2 WHERE user_id = $3 RETURNING *',
+      [highThreshold, lowThreshold, userId]
+    );
+
+    if (result.rows.length > 0) {
+      res.json(result.rows[0]);
+    } else {
+      res.status(404).json({ message: 'User settings not found' });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'An error occurred while updating user settings' });
+  }
+});
+
+module.exports = router;
 
