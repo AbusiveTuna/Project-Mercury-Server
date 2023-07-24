3ab42b8359435c71d1e9622f51e1045af0b0ed08
AbusiveTuna <89785560+AbusiveTuna@users.noreply.github.com>
2023-07-24 07:49:06 -0400

    Update dexcomRoutes.test.js

--- src/tests/dexcomRoutes.test.js
+++ src/tests/dexcomRoutes.test.js
@@ -1,1 +1,25 @@
+const request = require('supertest');
+const express = require('express');
+const app = express();
+const router = require('../routes/dexcomRoutes');
+app.use(express.json());
+app.use(router);
+
+describe('POST /exchangeCode', () => {
+  it('should exchange code and store tokens', async () => {
+    const res = await request(app)
+      .post('/exchangeCode')
+      .send({ code: 'mockCode', user_id: 'mockUserId' });
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toEqual({ message: 'Tokens exchanged and stored successfully' });
+  });
+
+  it('should handle error when exchanging code', async () => {
+    const res = await request(app)
+      .post('/exchangeCode')
+      .send({ code: 'mockCode', user_id: 'mockUserId' });
+    expect(res.statusCode).toEqual(500);
+    expect(res.body).toEqual({ error: 'Failed to exchange code' });
+  });
+});
 
