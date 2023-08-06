4b75b6b1e3cdab3a54c97be691545541e16c937c
Tuna <Tdb00096@gmail.com>
2023-08-06 09:49:51 -0400

    trying a node hue api

--- src/routes/hueRoutes.js
+++ src/routes/hueRoutes.js
@@ -2,6 +2,8 @@ import express from 'express';
 const router = express.Router();
 import pool from '../db/db.js';
 import fetch from 'node-fetch';
+const v3 = require('node-hue-api').v3;
+const LightState = v3.lightStates.LightState;
 
 router.post('/hueAuth', async (req, res) => {
   try {
@@ -39,16 +41,29 @@ router.get('/getHueTokens/:userId', async (req, res) => {
 router.post('/updateHueDevices/:userId', async (req, res) => {
   try {
     const { userId } = req.params;
-    const devices = req.body;
+    const tokenResult = await pool.query('SELECT * FROM hue_tokens WHERE user_id = $1', [userId]);
+    if (tokenResult.rows.length === 0) {
+      res.status(400).json({ message: 'No Hue tokens found for this user' });
+      return;
+    }
+    const { ip_address, username } = tokenResult.rows[0];
 
+    // Create a new API instance
+    const api = await v3.api.createLocal(ip_address).connect(username);
+
+    // Get all lights
+    const lights = await api.lights.getAll();
+
+    // Update the database with the light details
     await Promise.all(
-      devices.map((device) =>
+      lights.map((light) =>
         pool.query(
           'INSERT INTO hue_lights (user_id, lightname, rid, rtype) VALUES ($1, $2, $3, $4) ON CONFLICT (rid) DO UPDATE SET lightname=$2, rtype=$4',
-          [userId, device.lightname, device.rid, device.rtype]
+          [userId, light.name, light.id, 'Light']
         )
       )
     );
+
     res.json({ message: 'Device list updated successfully' });
   } catch (err) {
     console.error(err.message);
@@ -56,25 +71,32 @@ router.post('/updateHueDevices/:userId', async (req, res) => {
   }
 });
 
-
 router.get('/getHueDevices/:userId', async (req, res) => {
   try {
     const { userId } = req.params;
-    const result = await pool.query(
-      'SELECT * FROM hue_lights WHERE user_id = $1',
-      [userId]
-    );
-    if (result.rows.length === 0) {
-      res.status(400).json({ message: 'No Hue devices found for this user' });
+    const tokenResult = await pool.query('SELECT * FROM hue_tokens WHERE user_id = $1', [userId]);
+    if (tokenResult.rows.length === 0) {
+      res.status(400).json({ message: 'No Hue tokens found for this user' });
       return;
     }
-    const devices = result.rows.map((device) => device.lightname);
+    const { ip_address, username } = tokenResult.rows[0];
+
+    // Create a new API instance
+    const api = await v3.api.createLocal(ip_address).connect(username);
+
+    // Get all lights
+    const lights = await api.lights.getAll();
+
+    // Extract the light names
+    const devices = lights.map((light) => light.name);
+
     res.json(devices);
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server error');
   }
 });
+
 
 router.post('/toggleHueLight', async (req, res) => {
   try {
@@ -87,7 +109,7 @@ router.post('/toggleHueLight', async (req, res) => {
       res.status(400).json({ message: 'No Hue light found for this user' });
       return;
     }
-    const { rid, rtype } = result.rows[0];
+    const { rid } = result.rows[0];
     const tokenResult = await pool.query(
       'SELECT * FROM hue_tokens WHERE user_id = $1',
       [user_id]
@@ -96,26 +118,23 @@ router.post('/toggleHueLight', async (req, res) => {
       res.status(400).json({ message: 'No Hue tokens found for this user' });
       return;
     }
-    const { ip_address, username: hueUsername } = tokenResult.rows[0];
-    const url = `https://${ip_address}/clip/v2/resource/${rtype}/${rid}`;
-    const body = {
-      on: {
-        on,
-      },
-    };
-    await fetch(url, {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-        'hue-application-key': hueUsername,
-      },
-      body: JSON.stringify(body),
-    });
+    const { ip_address, username } = tokenResult.rows[0];
+
+    // Create a new API instance
+    const api = await v3.api.createLocal(ip_address).connect(username);
+
+    // Create a new light state
+    const lightState = new LightState().on(on);
+
+    // Set the light state
+    await api.lights.setLightState(rid, lightState);
+
     res.json({ message: `Light ${on ? 'turned on' : 'turned off'} successfully` });
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server error');
   }
 });
+
 
 export default router;
