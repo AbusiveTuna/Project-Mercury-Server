6d04d0a2709af37546b175f149a6b818e401f5e5
Tuna <Tdb00096@gmail.com>
2023-08-05 21:52:39 -0400

    testing URL

--- src/routes/hueRoutes.js
+++ src/routes/hueRoutes.js
@@ -5,10 +5,9 @@ import fetch from 'node-fetch';
 
 router.post('/hueAuth', async (req, res) => {
   try {
-    console.log(req.body);
     const { ipAddress, user_id } = req.body;
-    console.log(ipAddress, user_id);
     const url = `https://${ipAddress}/api`;
+    console.log(url);
     const body = {
       devicetype: 'projectmercury#webportal',
       generateclientkey: true,
